3ac8b866de90394b19a444a742481ecaadba61a1
Adithya S <adi.1994@yahoo.in>
2019-03-05 09:56:58 +0000

    Add new file

---
+++ instance-id_by_tags.py
@@ -0,0 +1,30 @@
+!/usr/bin/python
+  
+import sys
+from jsonpath_rw import jsonpath, parse
+import jsonpath
+import boto3
+import json
+vm = []
+client = boto3.client('resourcegroupstaggingapi', region_name='ap-southeast-1')
+
+response = client.get_resources(
+    TagFilters=[
+        {
+            'Key': 'owner',
+            'Values': [
+                sys.argv[1],
+            ]
+        },
+    ],
+    ResourceTypeFilters=[
+        'ec2:instance',
+    ]
+)
+
+jpath = parse("$.ResourceTagMappingList[*].ResourceARN")
+j_response = [match.value for match in jpath.find(response)]
+
+for arn in j_response:
+    tmp=(str(arn).split("/"))
+    print(tmp[1])
